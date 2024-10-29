7ce3d6431e95151f9459a64e162e5e986370b55e
Steph Elsley <161695703+Stephh-E@users.noreply.github.com>
2024-10-29 19:50:49 +1100

    express project hello world

---
+++ src/server.js
@@ -0,0 +1,32 @@
+// Purpose:
+// COnfigure the server, e.g.
+// - routes 
+// - middleware
+// - CORS
+// - debug logger setups
+// - connections to databases 
+// - connections to file storage
+
+const express = require("express");
+const app = express();
+
+// Server app configuration goes here
+// middleware, routes etcç
+
+//app.verb(path, callback);
+app.get("/", (request, response) => {
+    // response.send("<h1>Hello, world!</h1>");
+
+    response.json({
+        message:"Hello world!"
+    })
+
+});
+
+//Server app configuration is finished by this point 
+
+// Export the app so that other files can control when the server 
+// starts and stops
+module.exports = {
+    app
+}
